b424976fd3f012fefd2825eceb5e137f76e18aab
sedica3340 <sedica3340@gmail.com>
2024-06-12 10:31:35 +0900

    깜뽀난트 기초

---
+++ src/components/Bye.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Bye() {
+
+    return (
+        <>
+        <div>
+            <a href="https://www.naver.com/">네이버?</a>
+        </div>
+        </>
+    )
+}
+
+export default Bye;
