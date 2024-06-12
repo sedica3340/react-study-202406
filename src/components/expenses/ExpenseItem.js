c4387ef5c995ec741b7159bb0653a224c00ab974
sedica3340 <sedica3340@gmail.com>
2024-06-12 12:25:03 +0900

    이벤트바인딩

--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -3,19 +3,30 @@ import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 const ExpenseItem = ({ date, title, price }) => {
-
     const formattedPrice = new Intl.NumberFormat("ko-KR").format(price);
 
+    // 이벤트 핸들러 선언
+
+    const clickHandler = (e) => {
+        console.log("클릭함!");
+        console.log(
+            e.target.previousElementSibling.firstElementChild.textContent
+        );
+    };
+
     return (
-        <Card className='expense-item'>
-          <ExpenseDate exDate={date} />
-          <div className='expense-item__description'>
-            <h2>{title}</h2>
-            <div className='expense-item__price'>{formattedPrice}원</div>
-          </div>
+        <Card className="expense-item">
+            <ExpenseDate exDate={date} />
+            <div className="expense-item__description">
+                <h2>{title}</h2>
+                <div className="expense-item__price">{formattedPrice}원</div>
+            </div>
+            <button id="btn" onClick={clickHandler}>
+                버튼1
+            </button>
+            <button id='btn2' onMouseOver={e => alert('하하')}>버튼2</button>
         </Card>
-    )
-  
+    );
 };
 
 export default ExpenseItem;
