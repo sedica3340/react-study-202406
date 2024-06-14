0d4cefd74f5d63bbd764bea8bdbd4c24d0a1f729
sedica3340 <sedica3340@gmail.com>
2024-06-14 11:35:06 +0900

    새로운출발

--- src/App.js
+++ src/App.js
@@ -1,49 +1,19 @@
-import "./App.css";
-import React, { useState } from "react";
-import ExpenseList from "./components/expenses/ExpenseList";
-import NewExpense from "./components/new-expense/NewExpense";
+import React, { useState } from 'react';
+import CourseInput from './components/CourseGoals/CourseInput';
+import CourseList from './components/CourseGoals/CourseList';
+import './App.css';
+
 const App = () => {
-    const expenses = [
-        {
-            title: "치킨냄새맡음",
-            price: 8000,
-            date: new Date(2024, 6 - 1, 3),
-        },
-        {
-            title: "김밥라면",
-            price: 6000,
-            date: new Date(2023, 6 - 1, 7),
-        },
-        {
-            title: "음료수두병마심",
-            price: 5000,
-            date: new Date(2024, 6 - 1, 12),
-        },
-        {
-            title: "피자먹음",
-            price: 38800,
-            date: new Date(2022, 3 - 1, 4),
-        },
-        {
-            title: "전기파리채",
-            price: 15000,
-            date: new Date(2023, 5 - 1, 14),
-        },
-    ];
-
-    // 배열을 상태변수로 관리
-    const [expenseList, setExpenseList] = useState(expenses);
-
-    const onAddExpense = (userInput) => {
-        setExpenseList([... expenseList, userInput]);
-    }
-
-    return (
-        <>
-            <NewExpense onSave={onAddExpense}/>
-            <ExpenseList expenses={expenseList} />
-        </>
-    );
+  return (
+    <div>
+      <section id="goal-form">
+        <CourseInput />
+      </section>
+      <section id="goals">
+        <CourseList />
+      </section>
+    </div>
+  );
 };
 
 export default App;
