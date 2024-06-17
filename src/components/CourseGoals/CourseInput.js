9f889e1e56a39f4608529b7c41b5f29a8dc7cfc4
sedica3340 <sedica3340@gmail.com>
2024-06-17 09:18:58 +0900

    동적 인라인 스타일 변경

--- src/components/CourseGoals/CourseInput.js
+++ src/components/CourseGoals/CourseInput.js
@@ -8,30 +8,50 @@ const CourseInput = ({ onAdd }) => {
         text: "",
     });
 
+    // 입력값 검증을 통과했는지 여부 상태관리
+    const [isValid, setIsValid] = useState(true);
+
     const changeHandler = (e) => {
         setData({
             ...data,
             text: e.target.value,
         });
+        if (data.text.trim().length > 0) {
+            setIsValid(true);
+        } else {
+            setIsValid(false);
+        }
     };
 
     const formHandler = (e) => {
         e.preventDefault();
-        onAdd(data);
+
+        if (data.text.trim().length > 0) {
+            setIsValid(true);
+            onAdd(data);
+        } else {
+            setIsValid(false);
+        }
 
         setData({
             id: Math.random().toString(),
             text: "",
         });
-        
-
     };
 
     return (
         <form onSubmit={formHandler}>
             <div className="form-control">
                 <label>나의 목표</label>
-                <input onChange={changeHandler} type="text" value={data.text}/>
+                <input
+                    onChange={changeHandler}
+                    type="text"
+                    value={data.text}
+                    style={{
+                        backgroundColor: isValid ? "transparent" : "salmon",
+                        borderColor: isValid ? "black" : "red",
+                    }}
+                />
             </div>
             <Button type="submit">목표 추가하기</Button>
         </form>
