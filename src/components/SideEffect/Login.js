269fea442d3428e738378ce3d255f27ddd8b11dd
sedica3340 <sedica3340@gmail.com>
2024-06-18 10:28:34 +0900

    useEffect 2번쨰 파라미터 배열이란

--- src/components/SideEffect/Login.js
+++ src/components/SideEffect/Login.js
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Card from "../UI/Card";
 import styles from "./Login.module.css";
 import Button from "../UI/Button";
 
 const Login = ({ onLogin }) => {
-    // 사용자가 입력한 이메일값을 상태관리
+    console.log("렌더링 수행!");
+
+    // 사용자가 입력한 이메일을 상태관리
     const [enteredEmail, setEnteredEmail] = useState("");
     // 이메일 입력값이 정상인지 유무 확인
     const [emailIsValid, setEmailIsValid] = useState();
@@ -13,23 +15,16 @@ const Login = ({ onLogin }) => {
     const [enteredPassword, setEnteredPassword] = useState("");
     // 패스워드 입력값이 정상인지 유무 확인
     const [passwordIsValid, setPasswordIsValid] = useState();
-    // 이메일과 패스워드가 둘다 정상인지 유무 확인
+
+    // 이메일, 패스워드가 둘 다 정상인지 확인
     const [formIsValid, setFormIsValid] = useState(false);
 
     const emailChangeHandler = (e) => {
         setEnteredEmail(e.target.value);
-
-        setFormIsValid(
-            e.target.value.includes("@") && enteredPassword.trim().length > 6
-        );
     };
 
     const passwordChangeHandler = (e) => {
         setEnteredPassword(e.target.value);
-
-        setFormIsValid(
-            e.target.value.trim().length > 6 && enteredEmail.includes("@")
-        );
     };
 
     const validateEmailHandler = () => {
@@ -40,10 +35,19 @@ const Login = ({ onLogin }) => {
         setPasswordIsValid(enteredPassword.trim().length > 6);
     };
 
+    // 로그인 버튼을 눌렀을 때 이벤트 핸들러
     const submitHandler = (e) => {
         e.preventDefault();
+        // App.js에서 받은 로그인핸들러 호출
         onLogin(enteredEmail, enteredPassword);
     };
+
+    useEffect(() => {
+        console.log("useEffect call in Login.js");
+        setFormIsValid(
+            enteredPassword.trim().length > 6 && enteredEmail.includes("@")
+        );
+    }, [enteredEmail, enteredPassword]);
 
     return (
         <Card className={styles.login}>
