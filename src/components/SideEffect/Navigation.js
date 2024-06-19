46cb0795a6b4e06f76d9afbeebb47efd29c906a5
sedica3340 <sedica3340@gmail.com>
2024-06-19 10:11:59 +0900

    useContext

--- src/components/SideEffect/Navigation.js
+++ src/components/SideEffect/Navigation.js
@@ -1,39 +1,35 @@
-import React from 'react';
-import styles from './Navigation.module.css';
-import AuthContext from '../../store/auth-context';
+import React, { useContext } from "react";
+import styles from "./Navigation.module.css";
+import AuthContext from "../../store/auth-context";
 
 const Navigation = () => {
 
-  const loginPage = (onLogout) => (
-    <>
-      <li>
-        <a href="/">MyPage</a>
-      </li>
-      <li>
-        <a href="/">Admin</a>
-      </li>
-      <li>
-        <button onClick={onLogout}>Logout</button>
-      </li>
-    </>
-  );
+    const {onLogout, isLoggedIn} = useContext(AuthContext);
+    const loginPage = (onLogout) => (
+        <>
+            <li>
+                <a href="/">MyPage</a>
+            </li>
+            <li>
+                <a href="/">Admin</a>
+            </li>
+            <li>
+                <button onClick={onLogout}>Logout</button>
+            </li>
+        </>
+    );
 
-  const anonymousPage = <li><button>Sign Up</button></li>
+    const anonymousPage = (
+        <li>
+            <button>Sign Up</button>
+        </li>
+    );
 
-  return (
-    <AuthContext.Consumer>
-      {({ isLoggedIn, onLogout }) => {
-        // console.log(context);
-        return (
-          <nav className={styles.nav}>
-            <ul>
-              {isLoggedIn ? loginPage(onLogout) : anonymousPage}
-            </ul>
-          </nav>
-        );
-      }}
-    </AuthContext.Consumer>
-  );
+    return (
+        <nav className={styles.nav}>
+            <ul>{isLoggedIn ? loginPage(onLogout) : anonymousPage}</ul>
+        </nav>
+    );
 };
 
 export default Navigation;
