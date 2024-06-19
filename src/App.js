66e9317a87111bbad2c5988539a4caf24734fcb5
sedica3340 <sedica3340@gmail.com>
2024-06-19 10:40:18 +0900

    실습준비

--- src/App.js
+++ src/App.js
@@ -1,44 +1,10 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./App.css";
-import MainHeader from "./components/SideEffect/MainHeader";
-import Login from "./components/SideEffect/Login";
-import Home from "./components/SideEffect/Home";
-
-import AuthContext from "./store/auth-context";
+import TodoTemplate from './components/Todo/TodoTemplate';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-    const loginHandler = (email, password) => {
-        localStorage.setItem("login-flag", "1");
-        setIsLoggedIn(true);
-    };
-    // side effect 처리를 위한 함수
-    useEffect(() => {
-        const storedLoginFlag = localStorage.getItem("login-flag");
-        if(storedLoginFlag === '1') {
-            setIsLoggedIn(true);
-        }
-        
-    }, []);
-
-    const logoutHandler = () => {
-        localStorage.removeItem("login-flag");
-        setIsLoggedIn(false);
-    }
-
-    return (
-        <AuthContext.Provider value={{
-            isLoggedIn: isLoggedIn,
-            onLogout: logoutHandler,
-        }}>
-            <MainHeader />
-            <main>
-                {isLoggedIn && <Home />}
-                {!isLoggedIn && <Login onLogin={loginHandler} />}
-            </main>
-        </AuthContext.Provider>
-    );
+    return (<TodoTemplate></TodoTemplate>);
 };
 
 export default App;
