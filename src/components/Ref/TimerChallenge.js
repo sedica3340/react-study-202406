d2476108e644f703e67a4d9e55c5dde3ac2d1466
sedica3340 <sedica3340@gmail.com>
2024-06-20 11:20:29 +0900

    useRef를 통한 timer유지

--- src/components/Ref/TimerChallenge.js
+++ src/components/Ref/TimerChallenge.js
@@ -1,14 +1,23 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
+
+
 
 const TimerChallenge = ({ title, targetTime }) => {
     const [timerStared, setTimerStarted] = useState(false);
     const [timerExpired, setTimerExpired] = useState(false);
+    const timer = useRef();
+   
     const startHandler = (e) => {
-        setTimeout(() => {
+        setTimerExpired(false);
+        timer.current = setTimeout(() => {
             setTimerExpired(true);
         }, targetTime * 1000);
 
         setTimerStarted(true);
+    };
+    const stopHandler = (e) => {
+        clearTimeout(timer.current);
+        setTimerStarted(false);
     };
     return (
         <section className="challenge">
@@ -18,7 +27,7 @@ const TimerChallenge = ({ title, targetTime }) => {
                 {targetTime} second{targetTime > 1 ? "s" : ""}
             </p>
             <p>
-                <button onClick={startHandler}>
+                <button onClick={timerStared ? stopHandler : startHandler}>
                     {timerStared ? "Stop" : "Start"} Challenge
                 </button>
             </p>
