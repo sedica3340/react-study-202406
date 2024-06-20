566b7e4d8c55fcb94783d6e51f0e817b60a99350
sedica3340 <sedica3340@gmail.com>
2024-06-20 10:55:07 +0900

    타이머 시작이벤트 생성

--- src/components/Ref/TimerChallenge.js
+++ src/components/Ref/TimerChallenge.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TimerChallenge = ({ title, targetTime }) => {
+    const [timerStared, setTimerStarted] = useState(false);
+    const [timerExpired, setTimerExpired] = useState(false);
+    const startHandler = (e) => {
+        setTimeout(() => {
+            setTimerExpired(true);
+        }, targetTime * 1000);
+
+        setTimerStarted(true);
+    };
     return (
         <section className="challenge">
             <h2>{title}</h2>
-            <p className="challenge-time">{targetTime} second{targetTime > 1 ? 's' : ''}</p>
-            <p>
-                <button>Start Challenge</button>
+            {timerExpired && <p>You lost!</p>}
+            <p className="challenge-time">
+                {targetTime} second{targetTime > 1 ? "s" : ""}
             </p>
-            <p className="">Time is running... / Timer inactive</p>
+            <p>
+                <button onClick={startHandler}>
+                    {timerStared ? "Stop" : "Start"} Challenge
+                </button>
+            </p>
+            <p className={timerStared ? "active" : undefined}>
+                {timerStared ? "Time is running..." : "Timer inactive"}
+            </p>
         </section>
     );
 };
