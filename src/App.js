d542e4be4fe6a5bc0f4e8a7daaa44d629939d445
sedica3340 <sedica3340@gmail.com>
2024-06-21 09:31:33 +0900

    header작성

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React, { useEffect, useRef, useState } from "react";
 import Player from "./components/Ref/Player";
 import TimerChallenge from "./components/Ref/TimerChallenge";
+import Header from "./components/Food/Layout/Header";
 
 const App = () => {
     return (
         <>
-            <Player />
-            <div id="challenges">
-                <TimerChallenge title={"EASY"} targetTime={1} />
-                <TimerChallenge title={"NOT EASY"} targetTime={5}/>
-                <TimerChallenge title={"GETTING TOUGH"} targetTime={10}/>
-                <TimerChallenge title={"PROS ONLY"} targetTime={15}/>
-            </div>
+            <Header />
         </>
     );
 };
