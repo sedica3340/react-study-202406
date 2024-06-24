b9cbee696256274ca5434d6f16bd61b1b3de0276
sedica3340 <sedica3340@gmail.com>
2024-06-24 12:40:43 +0900

    애니메이션처리

--- src/components/Food/Layout/HeaderCartButton.js
+++ src/components/Food/Layout/HeaderCartButton.js
@@ -1,14 +1,31 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartIcon from "./CartIcon";
 import styles from "./HeaderCartButton.module.scss";
 import CartContext from "../../../store/cart-context";
 
 const HeaderCartButton = ({ onShow }) => {
-    const { button, icon, badge } = styles;
+
+    const { cartItems } = useContext(CartContext);
+
+    const [isBump, setIsBump] = useState(false);
+
+    const { button, icon, badge, bump } = styles;
 
     const { totalAmount } = useContext(CartContext);
+
+    useEffect(() => {
+        if(cartItems.length === 0) return;
+        setIsBump(true);
+        const timer = setTimeout(() => {
+            setIsBump(false);
+        },300)
+
+        return () => clearTimeout(timer);
+        
+
+    }, [cartItems])
     return (
-        <button className={button} onClick={onShow}>
+        <button className={`${button} ${isBump? bump : undefined} `} onClick={onShow}>
             <span className={icon}>
                 <CartIcon />
             </span>
