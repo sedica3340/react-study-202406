8f257815e1b07829da99641a8ef8f2de26d3804d
sedica3340 <sedica3340@gmail.com>
2024-07-02 10:57:30 +0900

    이래저래 뭐 더함 링크이동 spa 어쩌구

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
-import React, { useEffect, useRef, useState } from "react";
-import Header from "./components/Food/Layout/Header";
-import Meals from "./components/Food/Meals/Meals";
-import Cart from "./components/Food/Cart/Cart";
-import CartContext from "./store/cart-context";
-import CartProvider from "./store/CartProvider";
+import React from 'react';
+import Home from './components/RouteExample/pages/Home';
+import Products from './components/RouteExample/pages/Products';
+import { createBrowserRouter, RouterProvider} from 'react-router-dom';
 
+
+const router = createBrowserRouter([
+    { path: '/', element: <Home /> },
+    { path: '/products', element: <Products /> },
+]);
 const App = () => {
-    const [cartIsShown, setCartIsShown] = useState(false);
-
-    const showCartHandler = () => {
-        setCartIsShown(true);
-    };
-    const hideCartHandler = () => {
-        setCartIsShown(false);
-    };
-
-    return (
-        <CartProvider>
-            {cartIsShown && <Cart onClose={hideCartHandler} />}
-            <Header onShowCart={showCartHandler} />
-            <div id="main">
-                <Meals />
-            </div>
-        </CartProvider>
-    );
+  
+  return (
+    <RouterProvider router={router} />
+  );
 };
 
 export default App;
