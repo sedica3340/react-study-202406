a580642752087e1139a59ef12815121352cc5368
sedica3340 <sedica3340@gmail.com>
2024-07-02 11:21:59 +0900

    라우터 뭐시기 아웃렛 뭐시기

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import Home from './components/RouteExample/pages/Home';
-import Products from './components/RouteExample/pages/Products';
-import { createBrowserRouter, RouterProvider} from 'react-router-dom';
-
+import React from "react";
+import Home from "./components/RouteExample/pages/Home";
+import Products from "./components/RouteExample/pages/Products";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RootLayout from "./components/RouteExample/layout/RootLayout";
 
 const router = createBrowserRouter([
-    { path: '/', element: <Home /> },
-    { path: '/products', element: <Products /> },
+    {
+        path: "/",
+        element: <RootLayout />,
+        children: [
+            { path: "/", element: <Home /> },
+            { path: "/products", element: <Products /> },
+        ],
+    },
 ]);
 const App = () => {
-  
-  return (
-    <RouterProvider router={router} />
-  );
+    return <RouterProvider router={router} />;
 };
 
 export default App;
