9b739d07e461bf383705bbd20baf4be5e76b7df5
sedica3340 <sedica3340@gmail.com>
2024-07-02 11:28:42 +0900

    커스텀 에러 페이지

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Home from "./components/RouteExample/pages/Home";
 import Products from "./components/RouteExample/pages/Products";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RootLayout from "./components/RouteExample/layout/RootLayout";
+import ErrorPage from "./components/RouteExample/pages/ErrorPage";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <RootLayout />,
+        errorElement: <ErrorPage />,
         children: [
             { path: "/", element: <Home /> },
             { path: "/products", element: <Products /> },
---
+++ src/components/RouteExample/pages/ErrorPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ErrorPage = () => {
+  return (
+    <main>
+        <h1>페이지를 찾을 수 없습니다!</h1>
+        <p>요청하신 페이지의 주소를 다시 확인해주세요.</p>
+    </main>
+  )
+}
+
+export default ErrorPage
