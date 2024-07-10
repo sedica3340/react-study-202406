43202c230b1a1e603ac267f92122ceb72b1707b4
sedica3340 <sedica3340@gmail.com>
2024-07-10 09:46:33 +0900

    ㅇㅅㅇ

--- src/components/RouteExample/components/EventSkeleton.js
+++ src/components/RouteExample/components/EventSkeleton.js
@@ -1,11 +1,11 @@
 import React from "react";
 import styles from "./EventSkeleton.module.scss";
 
-const EventSkeleton = () => {
+const EventSkeleton = ({count}) => {
     return (
         <div className={styles.events}>
             <div className={styles.list}>
-                {Array.from(new Array(4)).map((_, index) => (
+                {Array.from(new Array(count)).map((_, index) => (
                     <div key={index} className={styles.skeleton}>
                         <div className={styles.imageSkeleton} />
                         <div className={styles.contentSkeleton}>
--- src/components/RouteExample/pages/Events.js
+++ src/components/RouteExample/pages/Events.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import EventList from "../components/EventList";
 import { useLoaderData, json } from "react-router-dom";
 import EventSkeleton from "../components/EventSkeleton";
@@ -8,14 +8,17 @@ const Events = () => {
     // loader가 리턴한 데이터 받아오기
     // const eventList = useLoaderData();
 
+    // 이벤트 목록 아래 박스 참조
+    const skeletonBoxRef = useRef();
+
     const [events, setEvents] = useState([]);
     const [loading, setLoading] = useState(false);
     const [currentPage, setCurrentPage] = useState(1);
     const [isFinish, setIsFinish] = useState(false);
 
+    const [skeletonCount, setSkeletonCount] = useState(4);
     const loadEvents = async () => {
-
-        if(isFinish) {
+        if (isFinish) {
             return;
         }
         setLoading(true);
@@ -29,36 +32,72 @@ const Events = () => {
         setCurrentPage((prev) => prev + 1);
 
         setIsFinish(totalCount === updatedEvents.length);
-
+        const restEventsCount = totalCount - updatedEvents.length;
+        setSkeletonCount(Math.min(4, restEventsCount));
     };
-    const scrollHandler = throttle((e) => {
-        if (
-            loading ||
-            window.innerHeight + document.documentElement.scrollTop >=
-                document.documentElement.offsetHeight
-        ) {
+    // const scrollHandler = throttle((e) => {
+    //     if (
+    //         loading ||
+    //         window.innerHeight + document.documentElement.scrollTop >=
+    //             document.documentElement.offsetHeight
+    //     ) {
+    //         return;
+    //     }
+    //     loadEvents();
+    // }, 2000);
+
+    // useEffect(() => {
+    //     loadEvents();
+    // }, []);
+
+    // useEffect(() => {
+    //     window.addEventListener("scroll", scrollHandler);
+
+    //     return () => {
+    //         window.removeEventListener("scroll", scrollHandler);
+    //         scrollHandler.cancel();
+    //     };
+    // }, [currentPage, loading]);
+
+    // 화면에 특정 박스가 보이면 다음페이지 로딩
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+
+            // 현재 감시하고 있는 타겟의 정보
+            console.log(entries[0]);
+            
+            if(!entries[0].isIntersecting || loading || isFinish) {
             return;
+            }
+            loadEvents();
+        }, {
+            // 감시대상의 요소가 50% 보일때 콜백을 실행
+            threshold: 0.5
+        });
+        if (skeletonBoxRef.current) {
+            observer.observe(skeletonBoxRef.current);
         }
-        loadEvents();
-    }, 2000);
+        
 
-    useEffect(() => {
-        loadEvents();
-    }, []);
-
-    useEffect(() => {
-        window.addEventListener("scroll", scrollHandler);
-
+        // 깜뽀난트가 렌더링이 사라질 때 옵저빙 중지
         return () => {
-            window.removeEventListener("scroll", scrollHandler);
-            scrollHandler.cancel();
-        };
-    }, [currentPage, loading]);
+            if(skeletonBoxRef.current) {
+                observer.disconnect();
+            }
+        }
+
+    }, [loading, currentPage]);
+
     return (
         <>
             <h1>Events Page</h1>
             <EventList eventList={events} />
-            {loading && <EventSkeleton />}
+            <div
+                ref={skeletonBoxRef}
+                style={{ height: "300px", background: "yellow" }}
+            >
+                {loading && <EventSkeleton count={skeletonCount} />}
+            </div>
         </>
     );
 };
